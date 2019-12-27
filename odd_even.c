20a73705726154e4e444ac9f884c4359eca3af19
sachinkreeti <sgupta@kreeti.com>
2019-12-27 14:47:41 +0530

    added header file, remove comment

--- odd_even.c
+++ odd_even.c
@@ -3,7 +3,6 @@ int main() {
     int num;
     printf("Enter an integer: ");
     scanf("%d", &num);
-    // True if num is perfectly divisible by 2
     if(num % 2 == 0)
         printf("%d is even.", num);
     else
