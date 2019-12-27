0b2d905e48d141dcca0208c17821cc54331d7cb8
sachinkreeti <sgupta@kreeti.com>
2019-12-27 14:41:43 +0530

    minor fixup

--- prime_number_program.c
+++ prime_number_program.c
@@ -3,7 +3,7 @@ int main() {
     int n, i, flag = 0;
     printf("Enter a positive integer: ");
     scanf("%d", &n);
-    for (i = 2; i <= n / 2; ++i) {
+    for (i = 2; i <= sqrt(n); ++i) {
         // condition for non-prime
         if (n % i == 0) {
             flag = 1;
